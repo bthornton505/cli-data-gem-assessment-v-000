091359dfbf57e83b6c674d5cbc4f891cb5d662c3
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-05 19:33:45 +0000

    moved code from market_scraper into market.rb. I was having issues getting it to work.

--- FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets.rb
+++ FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets.rb
@@ -1,7 +1,8 @@
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 require_relative "./farmers_markets/version"
 require_relative './farmers_markets/cli'
 require_relative './farmers_markets/market'
+require_relative './farmers_markets/market_scraper'
