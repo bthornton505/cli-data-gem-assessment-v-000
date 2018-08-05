53d871c06d1e34f1ab37a0dc7adb0118470db6fa
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-05 19:35:25 +0000

    I started to build my scraper in this method because I couldn't get it to work in my new object. Still not sure why...

--- FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets/market.rb
+++ FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets/market.rb
@@ -1,20 +1,26 @@
 class FarmersMarkets::Market 
-  attr_accessor :name 
+  attr_accessor :name, :time, :description, :market, :url 
   
   def self.all_markets 
+    self.scrape_markets
+  end 
+  
+  def self.scrape_markets
+    markets = []
+    markets << self.scrape_tripsavvy 
+    markets 
+  end 
+  
+  def self.scrape_tripsavvy
+    doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
+     
     
-    FarmersMarkets::MarketScraper.new 
+    name = doc.css("h3#text-block_9-0").first.text
+    time = doc.css("div#inline-chop_2-0 p")[1].text
+    description = doc.css("div#inline-chop_2-0 p").first.text
+    url = doc.css("div#inline-chop_2-0 p a").first.attr('href')
+    binding.pry
     
-    # market_1 = self.new 
-    # market_1.name = "Cherry Creek Fresh Market"
-    
-    # market_2 = self.new 
-    # market_2.name = "South Pearl Street Farmers Market"
-    
-    # market_3 = self.new 
-    # market_3.name = "City Park Esplanade Fresh Market"
-    
-    [market_1, market_2, market_3]
   end 
   
 end 
