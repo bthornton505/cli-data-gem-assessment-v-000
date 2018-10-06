83b9205bdf58e164957a29ff83c08416fce3cc51
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-06 02:28:13 +0000

    automatically backed up by learn

--- FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative "./farmers_markets/version"
-require_relative './farmers_markets/cli'
-require_relative './farmers_markets/market'
-require_relative './farmers_markets/market_scraper'
--- FarmersMarkets-CLI-app/farmers_markets/lib/farmers_markets/version.rb
+++
@@ -1,3 +0,0 @@
-module FarmersMarkets
-  VERSION = "0.1.0"
-end
